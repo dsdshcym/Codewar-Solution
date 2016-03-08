66b6c5147c3a0205fada24a67dbe36d605c491ad
dsdshcym <dsdshcym@gmail.com>
2016-03-08 11:57:13 +0800

    Improve solution for Your order, please

--- your_order_please.py
+++ your_order_please.py
@@ -1,14 +1,9 @@
-import re
-
 def order(sentense):
-    if not sentense:
-        return ""
-    words = sentense.split()
-    ans = [""] * len(words)
-    for word in words:
-        order = re.search(r"\d", word).group(0)
-        ans[int(order) - 1] = word
-    return " ".join(ans)
+    """
+    Digits will be sorted to the beginning of the sorted results
+    """
+    return " ".join(sorted(sentense.split(),
+                           key=lambda x: sorted(x)))
 
 assert(order("is2 Thi1s T4est 3a") == "Thi1s is2 3a T4est")
 print("tests passed")
