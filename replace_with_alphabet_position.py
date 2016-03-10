500e6ad1428cb1be01d25766f1d5c687b7b35e86
dsdshcym <dsdshcym@gmail.com>
2016-03-10 09:47:33 +0800

    Improve Replace with Alphabet Position

--- replace_with_alphabet_position.py
+++ replace_with_alphabet_position.py
@@ -1,6 +1,4 @@
-from string import ascii_lowercase
-
 def alphabet_position(text):
-    return ' '.join([str(ascii_lowercase.index(c) + 1)
-                    for c in text.lower()
-                    if c in ascii_lowercase])
+    return ' '.join([str(ord(c) - ord('a') + 1)
+                     for c in text.lower()
+                     if c.isalpha()])
