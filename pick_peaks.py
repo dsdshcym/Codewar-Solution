2a144cb2d4470a6e3bc315be4f07a548aca2f7dd
Yiming Chen <dsdshcym@promote.cache-dns.local>
2016-04-03 16:21:54 +0800

    Add best practice for Pick Peaks

--- pick_peaks.py
+++ pick_peaks.py
@@ -1,21 +1,14 @@
 def pick_peaks(arr):
-    arr = remove_duplicate(arr)
-    results = [arr[i]
-               for i in range(1, len(arr) - 1)
-               if is_peak(arr[i - 1][1], arr[i][1], arr[i + 1][1])]
-    print(arr, results)
-    return {
-        "pos": [pos for pos, _ in results],
-        "peaks": [peak for _, peak in results]
-    }
-
-def is_peak(a, b, c):
-    return (a < b > c)
-
-def remove_duplicate(arr):
-    return [(i, x)
-            for i, x in enumerate(arr)
-            if i == 0 or x != arr[i - 1]]
+    pos = []
+    prob_peak = None
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i - 1]:
+            prob_peak = i
+        else:
+            if arr[i] < arr[i - 1] and prob_peak:
+                pos.append(prob_peak)
+                prob_peak = None
+    return {"pos": pos, "peaks": [arr[i] for i in pos]}
 
 print(pick_peaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]))
 print(pick_peaks([12, 17, 13, 2, 19, 15, -3, -1, 17]))
