280ccf887c574b1676782ea73954141db9e005e4
Yiming Chen <dsdshcym@Yimings-MacBook-Pro.local>
2016-04-05 13:12:48 +0800

    Refactor base64_encoding to a more functional form

--- base64_encoding.py
+++ base64_encoding.py
@@ -6,46 +6,53 @@ BASE64_INDEX_TABLE = ''.join([string.ascii_uppercase,
                               '+',
                               '/'])
 
-def int_to_n_bits(x, n):
-    return bin(x)[2:].rjust(n, '0')
+def int_to_n_bits(n):
+    def f(x):
+        return bin(x)[2:].rjust(n, '0')
+    return f
 
-def plain_char_to_bit_pattern(char):
-    return int_to_n_bits(ord(char), 8)
+def split_string_in_fixed_length(length):
+    def f(string):
+        return [string[i: i+length] for i in range(0, len(string), length)]
+    return f
 
-def split_string_in_fixed_length(string, length):
-    return [string[i: i+length] for i in range(0, len(string), length)]
+def encode_bit_pattern(length, encode_function):
+    def f(bit_pattern):
+        return ''.join(map(encode_function,
+                           split_string_in_fixed_length(length)(bit_pattern)))
+    return f
 
-def bit_pattern_to_encoded_char(bits):
-    return BASE64_INDEX_TABLE[int(bits, 2)]
+def encode(string, split_function, to_bit_pattern, encode_function):
+    string_groups = split_function(string)
+    bit_patterns = [''.join(map(to_bit_pattern, group))
+                    for group in string_groups]
+    return ''.join(map(encode_function, bit_patterns))
 
-def bits_to_base_64(bits):
-    padded_bits = bits.ljust(24, '0')
-    results = ''.join(map(bit_pattern_to_encoded_char,
-                          split_string_in_fixed_length(padded_bits, 6)))
-    equal_sign_counts = (len(padded_bits) - len(bits)) // 6
-    return ''.join(results[:len(results)-equal_sign_counts])
+
+def _plain_char_to_bit_pattern(char):
+    return int_to_n_bits(8)(ord(char))
+
+def _bit_pattern_to_encoded_char(bits):
+    return BASE64_INDEX_TABLE[int(bits.ljust(6, '0'), 2)]
 
 def to_base_64(string):
-    bit_pattern = ''.join(map(plain_char_to_bit_pattern,
-                              string))
-    return ''.join(map(bits_to_base_64,
-                       split_string_in_fixed_length(bit_pattern, 24)))
+    return encode(string,
+                  split_string_in_fixed_length(3),
+                  _plain_char_to_bit_pattern,
+                  encode_bit_pattern(6,
+                                     _bit_pattern_to_encoded_char))
 
-def encoded_char_to_bit_pattern(char):
-    return int_to_n_bits(BASE64_INDEX_TABLE.index(char), 6)
+def _encoded_char_to_bit_pattern(char):
+    return int_to_n_bits(6)(BASE64_INDEX_TABLE.index(char))
 
-def decode_8_bits(bits):
+def _bit_pattern_to_decoded_char(bits):
     if len(bits) != 8:
         return ""
     return chr(int(bits, 2))
 
-def decode_4_chars(encoded_4_chars):
-    encoded_chars = encoded_4_chars.replace("=", "")
-    bit_pattern = ''.join(map(encoded_char_to_bit_pattern,
-                              encoded_chars))
-    return ''.join(map(decode_8_bits,
-                       split_string_in_fixed_length(bit_pattern, 8)))
-
 def from_base_64(string):
-    return ''.join(map(decode_4_chars,
-                       split_string_in_fixed_length(string, 4)))
+    return encode(string,
+                  split_string_in_fixed_length(4),
+                  _encoded_char_to_bit_pattern,
+                  encode_bit_pattern(8,
+                                     _bit_pattern_to_decoded_char))
