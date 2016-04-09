12dcc63f2a5eb6d277dbd28d7ea8e7380476a9e4
dsdshcym <dsdshcym@gmail.com>
2016-04-09 14:11:11 +0800

    Refactor Tic-Tac-Toe Checker

--- tic-tac-toe_checker.py
+++ tic-tac-toe_checker.py
@@ -1,24 +1,27 @@
-def check(to_check, value):
-    return any((set(row) == set([value])) for row in to_check)
+def wins(lines, value):
+    for line in lines:
+        if not any([ele != value for ele in line]):
+            return True
+    return False
 
 def isSolved(board):
     board_size = len(board)
-    to_check = board + zip(*board)
-    to_check.append([board[i][i] for i in range(board_size)])
-    to_check.append([board[i][board_size-i-1] for i in range(board_size)])
 
-    x_won = check(to_check, 1)
-    o_won = check(to_check, 2)
-    if x_won and o_won:
-        return 0
-    elif x_won:
-        return 1
-    elif o_won:
-        return 2
-    elif any((0 in row) for row in board):
+    rows = board
+    columns = zip(*board)
+    diagonal = [board[i][i] for i in range(board_size)]
+    anti_diagonal = [board[i][board_size-i-1] for i in range(board_size)]
+
+    lines = rows + columns + [diagonal, anti_diagonal]
+
+    for i in (1, 2):
+        if wins(lines, i):
+            return i
+
+    if any((0 in row) for row in board):
         return -1
-    else:
-        return 0
+
+    return 0
 
 print(isSolved([[1,1,1],
                 [0,2,2],
