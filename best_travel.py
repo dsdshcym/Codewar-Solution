89f1cb558682406acd5ad7dee41446482de1ddea
Yiming Chen <dsdshcym@gmail.com>
2016-04-10 13:28:01 +0800

    Refactor solution for Best Travel

--- best_travel.py
+++ best_travel.py
@@ -1,6 +1,9 @@
 from itertools import combinations
 
 def choose_best_sum(t, k, ls):
-    return max([sum(comb)
-                for comb in combinations(ls, k)
-                if sum(comb) <= t] or [None])
+    sums = [sum(comb)
+            for comb in combinations(ls, k)
+            if sum(comb) <= t]
+    if sums:
+        return max(sums)
+    return None
