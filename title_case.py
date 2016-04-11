0bb14ce22583c55389793a133eedbde9ad2efe2a
Yiming Chen <dsdshcym@gmail.com>
2016-04-11 15:01:25 +0800

    Refactor solution for Title Case

--- title_case.py
+++ title_case.py
@@ -1,13 +1,7 @@
-def capitalize(s, minor_words=[]):
-    s = s.lower()
-    if s in minor_words:
-        return s
-    return s[0].upper() + s[1:]
-
 def title_case(title, minor_words=''):
-    if not title:
-        return ''
-    minor_words = [s.lower() for s in minor_words.split()]
-    title = [capitalize(s, minor_words) for s in title.split()]
-    title[0] = capitalize(title[0])
-    return ' '.join(title)
+    title = title.lower().split()
+    minor_words = minor_words.lower().split()
+    return ' '.join(s
+                    if i and s in minor_words
+                    else s.capitalize()
+                    for i, s in enumerate(title))
